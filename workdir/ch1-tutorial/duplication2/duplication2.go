d44f03a9119c854a5dd19f51573ec05364da898c
nasikshafeek <nasik2ms@gmail.com>
2023-09-01 23:20:26 -0400

    Exercise 1.4: Adding the ability to show file paths for the given duplicated lines * Had do introduce custom made set data structure to handle de-duplication * Usage of Fprintf * Usage of map data structures

--- workdir/ch1-tutorial/duplication2/duplication2.go
+++ workdir/ch1-tutorial/duplication2/duplication2.go
@@ -4,16 +4,18 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // go run workdir/ch1-tutorial/duplication2/duplication2.go ./abc ./xyz
 
 func main() {
 	duplicateCounts := make(map[string]int)
+	duplicatedFileNames := make(map[string] []string)
 	files := os.Args[1:]
 
 	if (len(files) == 0) {
-		countLines(os.Stdin, duplicateCounts)
+		countLines(os.Stdin, duplicateCounts, duplicatedFileNames)
 	} else {
 		for _, filepath := range files {
 			aFile, openErr := os.Open(filepath)
@@ -24,15 +26,15 @@ func main() {
 				continue
 			}
 
-			countLines(aFile, duplicateCounts)
+			countLines(aFile, duplicateCounts, duplicatedFileNames)
 			aFile.Close()
 		}
 	}
 
-	printResults(duplicateCounts)
+	printResults(duplicateCounts, duplicatedFileNames)
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, counts map[string]int, duplicatedFileNames map[string][]string) {
 	input := bufio.NewScanner(f)
 
 	for input.Scan() {
@@ -41,13 +43,25 @@ func countLines(f *os.File, counts map[string]int) {
 		Usually when we do ++ operation it will do it on the zero value for the given type (for int it's 0)
 		*/
 		counts[input.Text()]++ // How would an empty value be incremented/handled?
+		
+		duplicatedFileNames[input.Text()] = append(duplicatedFileNames[input.Text()], f.Name())
 	}
 }
 
-func printResults(counts map[string]int) {
+func printResults(counts map[string]int,  duplicatedFileNames map[string][]string) {
 	for line, count := range counts {
 		if count > 1 {
-			fmt.Printf("%d\t%s\n", count, line)
+			// Deduplicating the file names if there are multiple files for a given line.
+			var uniqueFiles []string
+			uniqueFileNames :=  make(map[string] bool, 0)
+			for _, fileName := range duplicatedFileNames[line] {
+				uniqueFileNames[fileName] = true
+			}
+			for fileName := range uniqueFileNames {
+				uniqueFiles = append(uniqueFiles, fileName)
+			}
+			
+			fmt.Printf("%d\t%s\t%s\n", count, line, strings.Join(uniqueFiles, ", "))
 		}
 	}
 }
