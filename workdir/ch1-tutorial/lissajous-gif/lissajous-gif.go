9d5bf40972013c464dba740ad5b416f329a733c1
nasikshafeek <nasik2ms@gmail.com>
2023-09-05 00:33:11 -0400

    1.4 Animated GIFs * Implementing the Lissajous algorithm with the introduction to: ** Constants ** Floating point numbers ** Math package ** Loop within a loop ** GIF and image manipulation ** Struct and slice instantiation.

---
+++ workdir/ch1-tutorial/lissajous-gif/lissajous-gif.go
@@ -0,0 +1,53 @@
+// lissajous generates GIF animations of random Lissajous figures.
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+)
+
+var pallete = []color.Color{color.White, color.Black}
+
+const (
+	whiteIndex = 0 // first clor in pallete
+	blackIndex = 1 // next color in pallete
+)
+
+func main() {
+	lassojous(os.Stdout)
+}
+
+func lassojous(writer io.Writer) {
+	const (
+		nFrames = 64
+		size = 100
+		res = 0.001
+		cycles = 5
+		delay = 8
+	)
+
+	frequency :=  rand.Float64() * 3.5 // 
+	phase := 0.0;
+	anim := gif.GIF{LoopCount: nFrames}
+
+	for eachFrame := 0; eachFrame < nFrames; eachFrame ++ {
+		imageFrame := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(imageFrame, pallete)
+
+		for oscillator := 0.0; oscillator < 2*math.Pi*cycles; oscillator += res {
+			x := math.Sin(oscillator)
+			y := math.Sin(oscillator * frequency + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+		}
+		phase += 0.01
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+
+	gif.EncodeAll(writer, &anim)
+}
