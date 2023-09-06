6ee198ac87e9a5b2282b67912b020b428ac8f6d1
nasikshafeek <nasik2ms@gmail.com>
2023-09-05 21:11:47 -0400

    1.5 Fetch URLs: Excercise 1.8 * Modifying the function to add the http prefix if it does not exist.

---
+++ workdir/ch1-tutorial/fetch-copy-prefix/fetch-copy-prefix.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+)
+
+/*
+ * Adding a conditional to fetch-copy to find out if there exists the protocol in the URL, otherwise add it.
+ */
+func main() {
+	for _, url := range os.Args[1:] {
+
+		if !strings.HasPrefix(url, "http") {
+			url = "http://" + url
+		}
+
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch-copy-prefix: %v\n", err)
+			os.Exit(1)
+		}
+
+		_, copyErr := io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if copyErr != nil {
+			fmt.Fprintf(os.Stderr, "fetch-copy-prefix: reading %s: %v\n", url, copyErr)
+			os.Exit(1)
+		}
+	}
+}
