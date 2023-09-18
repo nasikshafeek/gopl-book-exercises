e7f8f27426211b085e1c75e930bfabc5d95495bc
nasikshafeek <nasik2ms@gmail.com>
2023-09-18 00:23:26 -0400

    Working around the aspect of channels and message passing in between goroutines

--- workdir/ch1-tutorial/fetchall-million/fetchall-million.go
+++ workdir/ch1-tutorial/fetchall-million/fetchall-million.go
@@ -4,7 +4,11 @@ import (
 	"encoding/csv"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
+	"time"
 )
 
 /**
@@ -12,14 +16,21 @@ import (
  */
 func main() {
 	// startTime := time.Now()
-	// fetchChannel := make(chan string)
+	fetchChannel := make(chan string)
 	csvReaderChannel := make(chan string)
 
-	go readCsv("./top-1m.csv", csvReaderChannel)
+	csvFilePath := filepath.Join(filepath.Dir("./"), "workdir/ch1-tutorial/fetchall-million/top-7.csv")
+	go readCsv(csvFilePath, csvReaderChannel)
 
 	// Ranging over a channel is an non terminal loop, until it receives a close message.
 	for channelMsg := range csvReaderChannel { 
 		fmt.Println("url from csvReaderChannel", channelMsg)
+		go fetch(channelMsg, fetchChannel)
+	}
+	// close(fetchChannel)
+
+	for fetchMsg := range fetchChannel {
+		fmt.Println("fetch msg", fetchMsg)
 	}
 
 	// for _, url := range os.Args[1:] {
@@ -35,7 +46,6 @@ func main() {
 	// fmt.Printf("%.2fs of total elapsed time\n", time.Since(startTime).Seconds())
 }
 
-/*
 func fetch(url string, channel chan<- string) {
 	start := time.Now()
 
@@ -48,19 +58,11 @@ func fetch(url string, channel chan<- string) {
 		channel <- fmt.Sprint(err) // Send message to the channel
 		return
 	}
-
-	nBytes, copyErr := io.Copy(io.Discard, resp.Body)
-	resp.Body.Close()
-
-	if copyErr != nil {
-		channel <- fmt.Sprint(os.Stderr, "fetch-copy-prefix: reading %s: %v\n", url, copyErr)
-		return
-	}
 	
+
 	secondsElapsed := time.Since(start).Seconds()
-	channel <- fmt.Sprintf("%.2fs %7d %s", secondsElapsed, nBytes, url)
+	channel <- fmt.Sprintf("%.2fs %v %s", secondsElapsed, resp.StatusCode, url)
 }
-*/
 
 func readCsv(filename string, readerChannel chan<- string) {
 	csvFile, err := os.Open(filename)
@@ -91,13 +93,10 @@ func readCsv(filename string, readerChannel chan<- string) {
 	for record, readErr := csvReader.Read(); readErr != io.EOF; record, readErr = csvReader.Read() {
 		url := record[1]
 		readerChannel <- url
+		// fmt.Printf("URL: %x\n", url)
 
-		// record, readErr = csvReader.Read()
-		// chunk, chunkReadError := csvReader.Read()
-		// fmt.Println("Read chunk", url, readErr);
-
-		// chunk, chunkReadError = csvReader.Read()
-		// fmt.Println("Read chunk", chunk, chunkReadError);
+		// Fetch the URL here and pass the message to the main callee
+		// go fetch(url, readerChannel)
 	}
 
 	// Always close the channel from the sender, not the receiver
